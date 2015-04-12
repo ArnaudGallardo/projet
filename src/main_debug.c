a3e41fa71a3f9875f0640b75ade653adb5f3840b
arnaud <arnaud@debian>
2015-04-12 19:57:48 -0400

    Maj main_debug

--- src/main_debug.c
+++ src/main_debug.c
@@ -11,8 +11,10 @@ int main(int argc, char *argv[])
 	add_tile(g);
 	add_tile(g);
 	draw_grid(g);
-	int i;
-	i = gps_arnaud(g);
-	do_move(g,i);
-	draw_grid(g);
+	long int i;
+	//i = gps_arnaud(g);
+	//do_move(g,i);
+	//draw_grid(g);
+	i=score(g);
+	printf("%ld",i);
 }
